9dd5caeeffb09ab4b1737a5d59caa2cf58592ca8
Enzo Degraeve <enzodeg40@gmail.com>
2024-02-24 13:50:33 +0100

    Refactor Discord OAuth implementation and update templates

--- discord.py
+++ discord.py
@@ -1,68 +1,61 @@
 import os
-
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
-
 import config as config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 app.secret_key = b"random bytes representing flask secret key"
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = config.discord_oauthlib_insecure_transport
 app.config["DISCORD_CLIENT_ID"] = config.discord_client_id
 app.config["DISCORD_CLIENT_SECRET"] = config.discord_client_secret
 app.config["DISCORD_REDIRECT_URI"] = config.discord_redirect_uri
-app.config["DISCORD_BOT_TOKEN"] = config.discord_bot_token
 
 discord = DiscordOAuth2Session(app)
 
 @app.route('/')
-def home():
-    return """
-    <html>
-        <head>
-            <title>Home</title>
-        </head>
-        <body>
-            <a href='/login/'>Login with Discord</a>
-        </body>
-    </html>"""
+def index():
+    return render_template("index.html")
 
-@app.route("/login/")
+@app.route("/home")
+@requires_authorization
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
 def login():
-    return discord.create_session()
+    if discord.authorized:
+        return redirect(url_for("home"))
+    else:  
+        return discord.create_session()
 
 @app.route('/logout')
 def logout():
     discord.revoke()
     return redirect(url_for('home'))
-	
 
 @app.route("/callback/")
 def callback():
     discord.callback()
-    return redirect(url_for(".me"))
-
+    return redirect(url_for("home"))
 
 @app.errorhandler(Unauthorized)
 def redirect_unauthorized(e):
     return redirect(url_for("login"))
-
 	
-@app.route("/me/")
-@requires_authorization
-def me():
-    user = discord.fetch_user()
-    return f"""
-    <html>
-        <head>
-            <title>{user.name}</title>
-        </head>
-        <body>
-            <img src='{user.avatar_url}' />
-        </body>
-    </html>"""
-
+# @app.route("/account/")
+# @requires_authorization
+# def me():
+#     user = discord.fetch_user()
+#     return f"""
+#     <html>
+#         <head>
+#             <title>{user.name}</title>
+#         </head>
+#         <body>
+#             <img src='{user.avatar_url}' />
+#         </body>
+#     </html>"""
 
 if __name__ == "__main__":
     app.run()
